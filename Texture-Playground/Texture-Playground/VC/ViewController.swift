cd5ac0dce26ab9c994c8de6fa7256cf548f06e62
Woopa01 <whdnwls49@naver.com>
2019-04-04 17:17:30 +0900

    [UPDATE] ViewController.swift
    button click 이벤트 추가

--- Texture-Playground/Texture-Playground/VC/ViewController.swift
+++ Texture-Playground/Texture-Playground/VC/ViewController.swift
@@ -8,6 +8,7 @@
 import AsyncDisplayKit
 import RxSwift
 import RxCocoa
+import RxASControlEvent
 
 class ViewController: ASViewController<ASDisplayNode> {
     
@@ -42,6 +43,9 @@ class ViewController: ASViewController<ASDisplayNode> {
         node.shadowColor = UIColor.black.withAlphaComponent(0.3).cgColor
         node.setTitle("페이스북으로 시작하기", with: UIFont.systemFont(ofSize: 15, weight: .medium), with: .white , for: UIControl.State.normal)
         node.backgroundColor = Color.BLUE.getColor()
+        node.rx.event(.touchDown).subscribe(onNext: { _ in
+            print("fbButtonDidClicked")
+        })
         return node
     }()
     
@@ -55,22 +59,33 @@ class ViewController: ASViewController<ASDisplayNode> {
         node.setTitle("이메일로 시작하기", with: UIFont.systemFont(ofSize: 15, weight: .medium), with: .black , for: UIControl.State.normal)
         node.backgroundColor = .white
         node.borderWidth = 0.5
+        node.rx.event(.touchDown).subscribe(onNext: { _ in
+            print("emailButtonDidClicked")
+        })
+        return node
+    }()
+    
+    lazy var signUpButtonNode : ASButtonNode = { () -> ASButtonNode in
+        let node = ASButtonNode()
+        node.setAttributedTitle(NSAttributedString(string: "이미 아이디가 있으신가요?"), for: .normal)
+        node.rx.event(.touchDown).subscribe(onNext: { _ in
+            self.dismiss(animated: true, completion: nil)
+            self.navigationController?.pushViewController(SignUpVC(), animated: true)
+        })
         return node
     }()
     
     
     init() {
         super.init(node: ASDisplayNode())
-        title = "hello!"
-        
         node.automaticallyManagesSubnodes = true
         node.layoutSpecBlock = {[weak self] (_,_) -> ASLayoutSpec in
             guard  let strongSelf = self else { return ASLayoutSpec() }
             strongSelf.titleNode.style.spacingBefore = 50.0
             strongSelf.desc1Node.style.spacingAfter = 5.0
-            strongSelf.desc2Node.style.spacingAfter = 300.0
+            strongSelf.desc2Node.style.spacingAfter = 250.0
             strongSelf.fbButtonNode.style.spacingAfter = 10.0
-            
+            strongSelf.emailButtonNode.style.spacingAfter = 10.0
             
             return ASStackLayoutSpec(direction: .vertical,
                                      spacing: 0.5,
@@ -80,9 +95,14 @@ class ViewController: ASViewController<ASDisplayNode> {
                                                 strongSelf.desc1Node,
                                                 strongSelf.desc2Node,
                                                 strongSelf.fbButtonNode,
-                                                strongSelf.emailButtonNode])
-           
+                                                strongSelf.emailButtonNode,
+                                                strongSelf.signUpButtonNode])
         }
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.isNavigationBarHidden = true
     }
     
     required init?(coder aDecoder: NSCoder) {
